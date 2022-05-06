03c5a75bb97c02072a59034e10bdfbfe2443cba5
Ezequiel Augusto Stanganelli <estanganelli@alu.favaloro.edu.ar>
2022-05-06 01:34:10 -0300

    Add UML

---
+++ TP3_INCUCAI_cmake/TP3_INCUCAI.cpp
@@ -0,0 +1,12 @@
+﻿// TP3_INCUCAI.cpp : Defines the entry point for the application.
+//
+
+#include "TP3_INCUCAI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ TP3_INCUCAI_cmake/TP3_INCUCAI.h
@@ -0,0 +1,8 @@
+﻿// TP3_INCUCAI.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
